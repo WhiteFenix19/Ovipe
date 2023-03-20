5d7eeb548bae9c2c7229104bdc1777266b1b6e0e
WhiteFenix19 <30911462+WhiteFenix19@users.noreply.github.com>
2023-03-20 19:56:57 -0400

    Update macro.js

--- macro.js
+++ macro.js
@@ -1,13 +1,18 @@
+window.contenedor = false;
 document.addEventListener("keypress", function(event) {
-    if (event.keyCode == 49) {
+    if (event.keyCode == 49){
+if (contenedor == false) {
+                dos();
+                contenedor = true;   }}
 
-                dos();}
     if (event.keyCode == 51) {
         cosas();
+        contenedor = false;
+
   };});
 
         function breeding() {
-            ewewewewewe = document.getElementsByClassName("pet").length;
+            ewewewewewe = document.getElementsByClassName("pet").length - 1;
             awawawa = Math.floor((Math.random() * (ewewewewewe - 1 + 1)) + 1);
             var we = document.getElementsByClassName("pet")[awawawa];
             console.log(awawawa);
@@ -16,22 +21,36 @@ document.addEventListener("keypress", function(event) {
             setTimeout(() => {
             var iwiwi = (document.getElementsByClassName("ui-button ui-corner-all ui-widget")[15]);
             iwiwi.click();
-            setTimeout(() => {
+          setTimeout(() => {
             history.back();
             setTimeout(() => {
             var owowo = (document.getElementsByClassName("ui-icon ui-icon-circle-triangle-e")[0]);
             owowo.click();
+            
             ;}, 700);
             }, 700);
             }, 700);
-            };;
-   
+
+
+            
+
+            
+            };
+
+        
         function dos()
         {
+            
             breedingtimer = setInterval(breeding, 3500);
+            $('body').css('background-color', '#125e20');
+            
         }
         function cosas()
         {
+            if (contenedor == true){
             clearInterval(breedingtimer);
-            alert("Hello! Mi trabajo es contener la macro, TQM, que te hayan salido bonitas las pets");
+            $('body').css('background-color', '#ffffff');}
+                else {
+                    alert("No hay nada que detener :3");
+                }
         }
